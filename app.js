63da582af528bad1197d66cb23ef4afb61df9a71
huan206 <huan.nguyen2261@gmail.com>
2021-12-25 16:50:48 +0700

    fix error connect database Tutorial 5

--- app.js
+++ app.js
@@ -40,40 +40,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const { MongoClient } = require('mongodb');
-
-async function main() {
-   
-    const uri = "mongodb://localhost:27017";
-
-    const client = new MongoClient(uri);
-
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
-
-        // Make the appropriate DB calls
-        await listDatabases(client);
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        // Close the connection to the MongoDB cluster
-        await client.close();
-    }
-}
-
-main().catch(console.error);
-
-/**
- * Print the names of all available databases
- * @param {MongoClient} client A MongoClient that is connected to a cluster
- */
-async function listDatabases(client) {
-    databasesList = await client.db().admin().listDatabases();
-
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://localhost:27017/test';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = app;
